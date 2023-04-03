e57a41ac943a43c0ea6787194abb021a1955729e
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-03 16:32:45 +0800

    fix: index.js sort() by _id -> by name

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,7 +7,7 @@ const Todo = require('../../models/todo')
 router.get('/', (req, res) => {
   Todo.find()
     .lean()
-    .sort({ _id: 'asc' }) // desc
+    .sort({ name: 'asc' }) // desc
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
