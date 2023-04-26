0838959c4b7d626c12ba05a424fe7a8c2a3335fa
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-26 18:09:00 +0800

    fix: users.js syntax fine tune

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -7,8 +7,11 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-})
+// 加入 middleware，驗證 request 登入狀態
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -41,11 +44,5 @@ router.post('/register', (req, res) => {
   })
     .catch(err => console.log(err))
 })
-
-// 加入 middleware，驗證 request 登入狀態
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
 
 module.exports = router
