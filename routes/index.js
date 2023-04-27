643a48c47331dbc9efdb1dafec1e5d050025d0d6
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-27 16:38:57 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,11 @@ const home = require('./modules/home')
 // 引入 todos 模組程式碼
 const todos = require('./modules/todos')
 const users = require('./modules/users') 
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos) // 加入驗證程序
 router.use('/users', users) 
+router.use('/', authenticator, home) // 加入驗證程序
 
 // 匯出路由器
 module.exports = router
