545cf5927a78d1001cbda60d34e5e5313bb621d0
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-28 11:54:14 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser') //引用 body-parser
 const methodOverride = require('method-override') // 載入 method-override
 const session = require('express-session') //載入express-session
 const usePassport = require('./config/passport')// 載入設定檔，要寫在 express-session 以後
+const flash = require('connect-flash')
 
 const routes = require('./routes')// 引用路由器
 require('./config/mongoose')
@@ -29,10 +30,13 @@ app.use(methodOverride('_method'))
 
 usePassport(app)// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 
+app.use(flash())
+
 app.use((req, res, next) => {
-  // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
